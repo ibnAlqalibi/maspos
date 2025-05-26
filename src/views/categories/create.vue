b9d9739509b88a2fd895953ef57f92e23d294482
ibnAlqalibi <ibnuabdul969@gmail.com>
2025-05-26 10:33:15 +0700

    move category/create.vue to categories/create.vue

---
+++ src/views/categories/create.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="w-full flex justify-center bg-[#ecf0f2] py-12">
+    <div class="w-full max-w-md bg-white rounded-[16px] shadow overflow-hidden">
+      <div class="w-full bg-[#F5F5F5] p-[24px]">
+        <span class="regular text-[20px] font-[500]">Tambah Kategori</span>
+      </div>
+
+      <form action="" @submit.prevent="create">
+        <div class="p-[24px]">
+          <label class="block text-sm font-medium text-gray-700 mb-1"
+            >Nama Kategori</label
+          >
+          <input
+            v-model="formData.name"
+            type="text"
+            id="name"
+            :class="[
+              'w-full px-3 py-2 border rounded-md bg-[#F5F5F5] focus:outline-none focus:ring-1 focus:ring-blue-500',
+              errors.name ? 'border-red-500' : 'border-gray-800',
+            ]"
+            placeholder="Produk"
+          />
+          <span v-if="errors.name" class="text-red-500 text-sm">{{
+            errors.name
+          }}</span>
+        </div>
+
+        <div class="flex space-x-4 border-t border-gray-200 p-[24px]">
+          <button
+            type="button"
+            @click="$router.push('/')"
+            class="flex-1 px-4 py-2 text-blue-600 bg-white border border-blue-600 rounded-md hover:bg-blue-50 focus:outline-none"
+          >
+            Batal
+          </button>
+          <button
+            type="submit"
+            class="flex-1 px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none"
+          >
+            Tambah
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useCategoriesStore } from "@/stores/categories.store.js";
+import { toast } from "vue3-toastify";
+
+export default {
+  data() {
+    return {
+      categoriesStore: useCategoriesStore(),
+      formData: {
+        name: null,
+      },
+      errors: {
+        name: null,
+      },
+    };
+  },
+  mounted() {
+    this.categoriesStore.fetch();
+  },
+  computed: {
+    isNameDuplicate() {
+      return this.categoriesStore.categories.some(
+        (category) => category.name === this.formData.name
+      );
+    },
+  },
+  methods: {
+    async create() {
+      this.errors.name = null;
+
+      if (!this.formData.name) {
+        this.errors.name = "Nama Kategori Wajib Diisi.";
+        return;
+      }
+
+      if (this.isNameDuplicate) {
+        this.errors.name = "Nama Kategori Sudah ada.";
+        toast("Nama Kategori Sudah ada.", {
+          autoClose: 2000,
+          type: "error",
+          position: toast.POSITION.TOP_RIGHT,
+        });
+        return;
+      }
+
+      let cat = await this.categoriesStore.add(this.formData);
+      if (cat) {
+        this.formData.name = null;
+        toast("Kategori Berhasil Ditambahkan", {
+          autoClose: 2000,
+          type: "success",
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      } else {
+        this.errors.name = "Gagal menambahkan kategori.";
+        toast("Gagal menambahkan kategori.", {
+          autoClose: 2000,
+          type: "error",
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+    },
+  },
+};
+</script>
